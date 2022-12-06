6028aafecfdde1a31f6863af665338a3ef76e2ae
Sophie LaBelle <sophie.r.labelle@gmail.com>
2022-12-05 19:09:55 -0800

    Add dragon and dragon-test

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,26 @@
+class Dragon {
+    constructor(name, rider, hungry) {
+        this.name = name;
+        this.rider = rider;
+        this.hungry = true;
+        this.counter = 0;
+    };
 
+    greet() {
+      return `Hi, ${this.rider}!`;
+    };
+
+    eat() {
+        if (this.counter < 2) {
+            this.counter += 1;
+        } else {
+            this.hungry = false;
+            delete this.counter;
+        };
+    };       
+
+};
+
+
+
+module.exports = Dragon;
